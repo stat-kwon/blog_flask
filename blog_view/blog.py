94fc6b59ec539920863ec98ede4e6d660e5ee748
stat-kwon <stat_kwon@naver.com>
2021-05-15 23:02:05 +0900

    blog.py 업데이트

--- blog_view/blog.py
+++ blog_view/blog.py
@@ -1,7 +1,18 @@
-from flask import Flask, Blueprint, request, render_template
+from flask import Flask, Blueprint, request, render_template, make_response, jsonify
+from werkzeug.utils import redirect
 
 blog_abtest = Blueprint('blog', __name__)
 
-@blog_abtest.route('/test')
-def test():
+@blog_abtest.route('/set_email', methods=['POST'])
+def set_email():
+    if request.method == 'GET':
+        print('set_email',request.args.get('user_email'))
+        return redirect('/blog/test_blog')
+    else:
+        print('set_email', request.headers)
+        print('set_email', request.get_json())
+        return redirect('/blog/test_blog')
+    
+@blog_abtest.route('/test_blog')
+def test_blog():
     return render_template('blog_A.html')
