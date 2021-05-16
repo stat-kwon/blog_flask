53f13cc714dd6b3096812f1b63ec90f4b44bacd4
stat-kwon <stat_kwon@naver.com>
2021-05-16 23:39:34 +0900

    blog.py 수정

--- blog_view/blog.py
+++ blog_view/blog.py
@@ -4,14 +4,6 @@ from werkzeug.utils import redirect
 blog_abtest = Blueprint('blog', __name__)
 
 @blog_abtest.route('/set_email', methods=['POST'])
-def set_email():
-    if request.method == 'GET':
-        print('set_email',request.args.get('user_email'))
-        return redirect('/blog/test_blog')
-    else:
-        print('set_email', request.headers)
-        print('set_email', request.get_json())
-        return redirect('/blog/test_blog')
     
 @blog_abtest.route('/test_blog')
 def test_blog():
